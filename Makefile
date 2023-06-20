9f8246474192ee133ddb99e93af963e4df0ea151
Paulo Andre Rodrigues De Bastos <paulorod@c1r15s7.42lisboa.com>
2023-06-20 12:58:45 +0100

    Added bonus files

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: paulorod <paulorod@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/06/05 12:49:38 by paulorod          #+#    #+#              #
-#    Updated: 2023/06/05 13:25:39 by paulorod         ###   ########.fr        #
+#    Updated: 2023/06/20 12:57:21 by paulorod         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,13 +16,17 @@ CFLAGS = -Wall -Wextra -Werror
 SRCS = client.c \
 	server.c
 BINS = client server
+BINS_BONUS = client_bonus server_bonus
 LIBFT_PATH = ./Libft
 LIBFT = $(LIBFT_PATH)/libft.a
 
 all: $(NAME)
 
 $(NAME): $(LIBFT) $(BINS)
+bonus: $(LIBFT) $(BINS_BONUS)
 $(BINS): %: %.c
+	$(CC) $(CFLAGS) -o $@ $< $(LIBFT)
+$(BINS_BONUS): %: %.c
 	$(CC) $(CFLAGS) -o $@ $< $(LIBFT)
 $(LIBFT):
 	make -C $(LIBFT_PATH) all
@@ -30,5 +34,6 @@ clean:
 	make -C $(LIBFT_PATH) clean
 fclean: clean
 	rm -f $(BINS)
+	rm -f $(BINS_BONUS)
 	make -C $(LIBFT_PATH) fclean
 re: fclean all
---
+++ client_bonus.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paulorod <paulorod@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/23 15:32:09 by paulorod          #+#    #+#             */
+/*   Updated: 2023/06/20 12:52:06 by paulorod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+/*Count characters that the server received
+Once it prints the count exit the client*/
+void	signal_handler(int signum)
+{
+	if (signum == SIGUSR2)
+	{
+		ft_printf("%sServer received message.%s\n", GREEN, RESET);
+		exit(0);
+	}
+}
+
+/*Send the string to the server by converting the characters to binary
+SIGUSR1 = 0s & SIGUSR2 = 1s*/
+void	send_data(int pid, char *str)
+{
+	int		i;
+	int		j;
+	char	c;
+
+	j = 0;
+	while (str[j])
+	{
+		i = 8;
+		c = str[j++];
+		while (i--)
+		{
+			if (c >> i & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			usleep(100);
+		}
+	}
+	i = 8;
+	while (i--)
+	{
+		kill(pid, SIGUSR1);
+		usleep(100);
+	}
+}
+
+/*Initialize client*/
+int	main(int argc, char **argv)
+{
+	int	server_pid;
+
+	if (argc != 3)
+	{
+		ft_printf("%sInvalid number of arguments!%s\n", RED, RESET);
+		return (1);
+	}
+	ft_printf("Sending \"%s%s%s\" to server %s%s%s\n",
+		YELLOW, argv[2], RESET,
+		CYAN, argv[1], RESET);
+	server_pid = ft_atoi(argv[1]);
+	signal(SIGUSR1, signal_handler);
+	signal(SIGUSR2, signal_handler);
+	send_data(server_pid, argv[2]);
+	while (true)
+		pause();
+	return (0);
+}
